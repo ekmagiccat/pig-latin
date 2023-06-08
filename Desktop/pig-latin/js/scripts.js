beeda994d7b6f7ce34318b665bf5403d484544b1
Eva Kemp <eva.j.kemp@gmail.com>
2023-06-08 14:48:17 -0700

    add html
    Co-authored-by: Lindsay Warr <iamalion@gmail.com>

---
+++ Desktop/pig-latin/js/scripts.js
@@ -0,0 +1,20 @@
+function pigLatin(word) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  const wordArray = word.toLowerCase().split("");
+  for (let i = 0; i < wordArray.length; i++) {
+    if (vowels === i[0]) {
+      return word + "way";
+    }
+  }
+  return word;
+}
+-----
+function pigLatin(word) {
+    const wordArray = word.toLowerCase().split("");
+    for (let i = 0; i < wordArray.length; i++) { console.log(i)
+      if (i[0]=== "a"|| i[0] === "e" || i[0]==="i" || i[0]==="o" || i[0]==="u") {
+        return word + "way";
+      }
+    }
+    return word;
+  }
